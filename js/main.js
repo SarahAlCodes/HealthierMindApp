87168cf37467e552c08829b96d52d3977dd1f1b2
SarahAlCodes <salsaid1016@gmail.com>
2022-07-25 20:07:24 -0400

    Added navbar media queries

--- js/main.js
+++ js/main.js
@@ -6,6 +6,14 @@ const cards = document.querySelectorAll('.card');
 });
 
 
+const toggleButton = document.getElementsByClassName('toggle-button')[0]
+const navLinks = document.getElementsByClassName('nav-links')[0]
+
+toggleButton.addEventListener('click', () => {
+  navLinks.classList.toggle('active')
+})
+
+
 // play/pause background audio
 const audio = document.getElementById("audio");
 document.querySelector('.fa-circle-play').addEventListener('click', playPause)
@@ -17,4 +25,23 @@ function playPause() {
       audio.pause();
     }
 }
+
+
+
+// function play() {
+//   audio.play();
+// }
+// document.querySelector('.fa-pause').addEventListener('click', pause)
+// function pause() {
+//   audio.pause();
+// }
+
+// function myFunction() {
+//     let x = document.querySelector(".nav__list");
+//     if (x.style.display === "block") {
+//       x.style.display = "none";
+//     } else {
+//       x.style.display = "block";
+//     }
+//   }
 
